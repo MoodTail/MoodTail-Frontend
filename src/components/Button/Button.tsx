eec9b906b428e23abb270d34e3ed2d856e3dd588
안정현 <anjeonghyeon@anjeonghyeon-ui-MacBookPro.local>
2026-07-13 18:39:05 +0900

    feat: Button 공통 컴포넌트 구현

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import type { ButtonHTMLAttributes } from "react";
 import "../../styles/Button.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "text" | "light";
+  variant?: "primary" | "text" | "light" | "share" | "shareLight" | "result";
 }
 
 function Button({
