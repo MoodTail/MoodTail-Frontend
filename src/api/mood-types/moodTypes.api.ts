35068e35cc02fee18fcf4c1778e0ebb2de871caf
d4b1n <shekqsl1004@gmail.com>
2026-08-03 23:08:56 +0900

    Revert "feat: 결과화면용 무드테스트/무드타입 API 연동 계층 구현"
    This reverts commit 99d51d814adfdf4520a1464eb0d16d2c17ef8c04.

--- src/api/mood-types/moodTypes.api.ts
+++ src/api/mood-types/moodTypes.api.ts
@@ -1,11 +1,1 @@
-import { apiClient } from "../client";
-import type { GetMoodTypeDetailResponse, MoodTypeDetail } from "./moodTypes.types";
-
-export const getMoodTypeDetail = async (
-  moodTypeId: number,
-): Promise<MoodTypeDetail> => {
-  const response = await apiClient.get<GetMoodTypeDetailResponse>(
-    `/api/v1/mood-types/${moodTypeId}`,
-  );
-  return response.data.result;
-};
+export {};
